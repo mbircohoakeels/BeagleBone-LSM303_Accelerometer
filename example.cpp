cf4d72d2519b76eaf2cf429dc84a9efc6bc53f9f
Michael Brookes <mbircohoakeels@me.com>
2016-05-08 18:13:05 +0100

    Adding/Updating new example for Accelerometer

--- example.cpp
+++ example.cpp
@@ -1,8 +1,29 @@
-#include <iostream>
+
+#include "Lib/LSM303/Accelerometer/LSM303Accelerometer.h"
 
 using namespace std;
 
 int main( ) {
-    cout << "Hello, World!" << endl;
+    using namespace I2C;
+
+    LSM303Accelerometer *Accelerometer;
+
+    Accelerometer = new LSM303Accelerometer( );
+
+    Accelerometer->LoadRecommendedFlightSettings( );
+
+    Accelerometer->SetDataTimer( );
+
+    //while( 1 ) {
+        Accelerometer->X( );
+        Accelerometer->Y( );
+        Accelerometer->Z( );
+
+        cout << "Refresh Rate = " << Accelerometer->DataTimer << endl;
+        //cout << "X = " << bitset<16>( Accelerometer->Position->X ) << " = " << Accelerometer->Position->X << endl;
+        //cout << "Y = " << bitset<16>( Accelerometer->Position->Y ) << endl;
+        //cout << "Z = " << bitset<16>( Accelerometer->Position->Z ) << endl;
+        //usleep( 5000 );
+    //}
     return 0;
 }
